29be57f77658f12c1bc829a9e915029f262d5d5f
Milan M J <milanmj2004@gmail.com>
2025-03-15 23:54:49 +0530

    Delete Toturial 2/1.py

--- Toturial 2/1.py
+++
@@ -1,5 +0,0 @@
-# Write a program to remove all vowel characters from a string.
-s = input("Enter a string: ")
-vowels = "aeiouAEIOU"
-result = "".join(char for char in s if char not in vowels)
-print("String without vowels:", result)
